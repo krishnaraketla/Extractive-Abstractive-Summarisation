11e3daa6f93597a37ae05adc201e9828439603c5
raketlak <krishrak97@gmail.com>
2023-12-05 15:12:06 -0500

    v2

--- BERT_Summarizer/bertabs/modeling_bertabs.py
+++ BERT_Summarizer/bertabs/modeling_bertabs.py
@@ -24,7 +24,7 @@ import math
 
 import numpy as np
 import torch
-from configuration_bertabs import BertAbsConfig
+from bertabs.configuration_bertabs import BertAbsConfig
 from torch import nn
 from torch.nn.init import xavier_uniform_
 
